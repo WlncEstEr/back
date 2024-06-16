4e334921df7e8d4f31196b0d065ae160a9843695
Mike Gosteev <winchester@MacBook-Pro-Mike.local>
2024-06-16 23:26:46 +0300

    8 try fix refreshToken

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -20,19 +20,20 @@ export class AuthController {
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('login')
-	async login(@Body() dto: AuthDto, @Res() res: Response) {
+	async login(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
 		const { refreshToken, ...response } = await this.authService.login(dto)
 		this.authService.addRefreshTokenToResponse(res, refreshToken)
 
 		return response
 	}
 
-	// { passthrough: true }
-
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('register')
-	async register(@Body() dto: AuthDto, @Res() res: Response) {
+	async register(
+		@Body() dto: AuthDto,
+		@Res({ passthrough: true }) res: Response
+	) {
 		const { refreshToken, ...response } = await this.authService.register(dto)
 		this.authService.addRefreshTokenToResponse(res, refreshToken)
 
