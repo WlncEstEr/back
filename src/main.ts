87d30c8bb00ffe3f346c04f01583dc4bbd7cd249
Mike Gosteev <winchester@MacBook-Pro-Mike.local>
2024-06-16 23:04:23 +0300

    Six try fix refreshToken

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:3000', 'http://87.228.8.156:3000'],
+		origin: ['http://87.228.8.156:3000'],
 		credentials: true,
 		exposedHeaders: 'set-cookie',
 	})
