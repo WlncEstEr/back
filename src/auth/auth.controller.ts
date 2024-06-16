5a1988b900748b9ccbf2fd797af83609c529b6d8
Mike Gosteev <winchester@MacBook-Pro-Mike.local>
2024-06-16 23:25:01 +0300

    Seven try fix refreshToken

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,7 +7,7 @@ import {
 	Res,
 	UnauthorizedException,
 	UsePipes,
-	ValidationPipe
+	ValidationPipe,
 } from '@nestjs/common'
 import { Request, Response } from 'express'
 import { AuthService } from './auth.service'
@@ -20,20 +20,19 @@ export class AuthController {
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('login')
-	async login(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
+	async login(@Body() dto: AuthDto, @Res() res: Response) {
 		const { refreshToken, ...response } = await this.authService.login(dto)
 		this.authService.addRefreshTokenToResponse(res, refreshToken)
 
 		return response
 	}
 
+	// { passthrough: true }
+
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('register')
-	async register(
-		@Body() dto: AuthDto,
-		@Res({ passthrough: true }) res: Response
-	) {
+	async register(@Body() dto: AuthDto, @Res() res: Response) {
 		const { refreshToken, ...response } = await this.authService.register(dto)
 		this.authService.addRefreshTokenToResponse(res, refreshToken)
 
